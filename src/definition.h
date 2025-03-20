3b3e36f68d281a485fac9f5de0745132ad66de36
William Guay <2430063@collegebart.ca>
2025-03-20 10:06:25 -0400

    Ajouté quelques implémentations fonctionnelles

--- src/definition.h
+++ src/definition.h
@@ -5,19 +5,31 @@
 
 #include <stdint.h>
 
-typedef struct {
+typedef struct Item{
     char* nom;
     int valeur;
 } Item;
+
+typedef struct Node {
+    Item Item;
+    Node* Next;
+} Node;
+
+typedef struct Inventory {
+    Node* Head;
+} Inventory;
+
+
 
 void* allocate(size_t size);
 // Vous devez definir les structures et fonctions que vous voulez exposer dans les "headers"
 // Si jamais vous avez des fonctions avec des noms commun, vous devez les prefixer pour eviter des conflits de symboles
 // Le prefix peut etre le nom de votre projet, exemple: forhonor_nom_de_fonction
 
-void monprojet_ajouter_item(Item item);
-void monprojet_enlever_item(char* nom);
+void monprojet_ajouter_item(Inventory* inv, Item item);
+void monprojet_enlever_item(Inventory* inv, char* nom);
 void monprojet_trier_inventaire(void);
 int monprojet_get_quantite(void);
-Item* monprojet_trouver_item_par_nom(char* nom);
-Item* monprojet_trouver_item_par_position(int position);
+Item* monprojet_trouver_item_par_nom(Inventory* inv, char* nom);
+Item* monprojet_trouver_item_par_position(Inventory* inv, int position);
+Inventory* monprojet_CreerInventory(Item firstItem);
--- src/implementation.c
+++ src/implementation.c
@@ -6,10 +6,19 @@
 // Si vous le faites dans le fichier "header", chaque fichier qui l'include va compiler l`implementation
 // et cela causeras des definitions de symboles multiples.
 
-void monprojet_ajouter_item(Item item){
+void monprojet_ajouter_item(Inventory* inv, Item item){
+	Node* CurrentSlot = inv->Head;
+	while (CurrentSlot->Next != NULL)
+	{
+		CurrentSlot = CurrentSlot->Next;
+	}
+	Node* NewSlot = (Node*)allocate(sizeof(Node));
+	NewSlot->Item = item;
+	NewSlot->Next = NULL;
 
+	CurrentSlot->Next = NewSlot;
 };
-void monprojet_enlever_item(char* nom) {
+void monprojet_enlever_item(Inventory* inv, char* nom) {
 
 }
 void monprojet_trier_inventaire(void) {
@@ -18,9 +27,27 @@ void monprojet_trier_inventaire(void) {
 int monprojet_get_quantite(void) {
 
 }
-Item* monprojet_trouver_item_par_nom(char* nom) {
+Item* monprojet_trouver_item_par_nom(Inventory* inv, char* nom) {
+	Node* CurrentSlot = inv->Head;
+	while (CurrentSlot->Item.nom != nom)
+	{
+		if (CurrentSlot == NULL) {
+			printf("Couldnt find item by name.");
+			return;
+		}
+		CurrentSlot = CurrentSlot->Next;
+	}
+	return CurrentSlot;
+}
+Item* monprojet_trouver_item_par_position(Inventory* inv, int position) {
+	// Position 0 = head
 
 }
-Item* monprojet_trouver_item_par_position(int position) {
 
+Inventory* monprojet_CreerInventory(Item firstItem) {
+	Inventory* inv = (Inventory*)allocate(sizeof(Inventory));
+	inv->Head = (Node*)allocate(sizeof(Node));
+	inv->Head->Item = firstItem;
+	inv->Head->Next = NULL;
+	return inv;
 }
